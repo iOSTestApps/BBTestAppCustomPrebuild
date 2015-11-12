baf086f82518230c160255a455f8d268b0568899
Christopher Stott <christopherstott@gmail.com>
2015-11-12 11:20:52 -0800

    Initial Commit

---
+++ BBTestAppCustomPrebuildTests/BBTestAppCustomPrebuildTests.m
@@ -0,0 +1,39 @@
+//
+//  BBTestAppCustomPrebuildTests.m
+//  BBTestAppCustomPrebuildTests
+//
+//  Created by Chris on 2015-11-12.
+//  Copyright © 2015 Chris. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BBTestAppCustomPrebuildTests : XCTestCase
+
+@end
+
+@implementation BBTestAppCustomPrebuildTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
